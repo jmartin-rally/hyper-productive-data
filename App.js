bc8a03fb0156cfad92af2223effecb0e9c4ba52f
John Martin <curmudgeon@rallydev.com>
2013-03-08 17:32:42 -0800

    change found to estimate change

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     defaults: { padding: 5 },
     items: [
-        {xtype:'container', itemId: 'iteration_selector_box'},
+        {xtype:'container', itemId: 'selectors_box', layout: { type: 'hbox' } },
         {xtype:'container',itemId:'summary_grid_box'},
         {xtype: 'container', itemId: 'separator', margin: 25, padding: 25, html: "--" },
         {xtype:'container',itemId:'artifact_grid_box'}
@@ -12,9 +12,10 @@ Ext.define('CustomApp', {
     date_array: [],
     launch: function() {
         this._addIterationSelector();
+        this._addExportButton();
     },
     _addIterationSelector: function() {
-        this.down('#iteration_selector_box').add({ 
+        this.down('#selectors_box').add({ 
             xtype:'rallyiterationcombobox', 
             listeners: {
                 change: function(box,newValue) {
@@ -26,6 +27,18 @@ Ext.define('CustomApp', {
                 },
                 scope: this
             }
+        });
+    },
+    _addExportButton: function() {
+        this.down('#selectors_box').add({
+            xtype:'rallybutton',
+            text:'Export',
+            handler: function() {
+                if (this.summary_grid) {
+                    Rally.pxs.util.GridExporter.exportGrid(this.summary_grid);
+                }
+            },
+            scope: this
         });
     },
     _getIterations: function(iteration_name) {
@@ -72,8 +85,9 @@ Ext.define('CustomApp', {
         }
     },
     _getEndOfOneDaySnaps: function(date_array,iteration_ids) {
-        window.console && console.log( "_getEndOfOneDaySnaps");
+        window.console && console.log( "_getEndOfOneDaySnaps", date_array);
         var midnight = date_array.shift();
+
         var query = [{property:'Iteration',operator:'in',value:iteration_ids},{property:'__At',value:midnight}];
         Ext.create('Rally.data.lookback.SnapshotStore',{
             autoLoad: true,
@@ -103,13 +117,45 @@ Ext.define('CustomApp', {
                     if ( date_array.length > 0 ) {
                         this._getEndOfOneDaySnaps(date_array,iteration_ids);
                     } else {
-                        this._showSummary();
-                        this._showDetails();
+                        this._getCurrentSnaps(iteration_ids);
                     }
                 },
                 scope: this
             }
         });
+    },
+    _getCurrentSnaps: function(iteration_ids) {
+        window.console && console.log('_getCurrentSnaps');
+        var artifact_ids = [];
+        for ( var id in this.artifacts ) {
+            if ( this.artifacts.hasOwnProperty(id) ) {
+                artifact_ids.push(parseInt(id,10));
+            }
+        }
+        var query = [{property:'ObjectID',operator:'in',value:artifact_ids},{property:'__At',value:'current'}];
+
+        Ext.create('Rally.data.lookback.SnapshotStore',{
+            autoLoad:true,
+            fetch:  [
+                '_PreviousValues','Iteration','ObjectID','Name','PlanEstimate',
+                'c_cfEffortNotAccepted','c_cfFoundWork','ScheduleState'
+            ],
+            hydrate: ['ScheduleState'],
+            filters: query,
+            listeners: {
+                load: function(store,data,success) {
+                    window.console && console.log(data);
+                    var me = this;
+                    Ext.Array.each(data,function(snap) {
+                        me.artifacts[snap.get('ObjectID')].set('LastPlanEstimate',snap.get('PlanEstimate'));
+                    });
+                    this._showSummary();
+                    this._showDetails();
+                },
+                scope: this
+            }
+        });
+
     },
     _wasThereOnDayOne: function(artifact) {
         var there_on_day_one = false;
@@ -119,7 +165,7 @@ Ext.define('CustomApp', {
         return there_on_day_one;
     },
     _showSummary:function() {
-        window.console && console.log("_showSummary",this.date_array[0],this.artifacts);
+        window.console && console.log("_showSummary");
         var me = this;
         var original_commitment = 0;
         var velocity = 0;
@@ -132,8 +178,9 @@ Ext.define('CustomApp', {
                 
                 if ( me._wasThereOnDayOne(artifact) ) {
                     original_commitment += artifact.get('FirstPlanEstimate');
+                    found_estimate += artifact.get('EstimateDelta');
                 }
-                found_estimate += artifact.get('cfFoundWork');
+                
                 
                 if ((artifact.get('ScheduleState') === "Accepted")&&(me._wasThereOnDayOne(artifact)) ) {
                     velocity += artifact.get('FirstPlanEstimate');
@@ -159,7 +206,7 @@ Ext.define('CustomApp', {
             original_commitment: "&Sigma;estimate day one",
             velocity: "&Sigma;estimate day one for accepted",
             effort: "&Sigma;cfEffortNotAccepted for not accepted",
-            found_estimate: "&Sigma;cfFoundWork",
+            found_estimate: "&Sigma;(current-original estimates)",
             adopted_estimate: "&Sigma;estimate items not on day one",
             capacity_estimate: "velocity+estimate",
             total_commitment: "original commitment + adopted estimate + found estimate",
@@ -207,16 +254,17 @@ Ext.define('CustomApp', {
         this.down('#summary_grid_box').add(this.summary_grid);
     },
     _showDetails:function(){
-        window.console && console.log(this.date_array[0],this.artifacts);
+        window.console && console.log("_showDetails");
         var temp_array = [];
         var me = this;
         var columns = [
             {text: "Name", dataIndex: "Name", flex: 1 },
-            {text: "OriginalEstimate", dataIndex: 'FirstPlanEstimate'},
-            {text: "LastEstimate", dataIndex: 'LastPlanEstimate'},
-            {text: "ScheduleState", dataIndex: 'ScheduleState'},
-            {text: 'cfEffortNotAccepted', dataIndex: 'cfEffortNotAccepted'},
-            {text: 'cfFoundWork', dataIndex: 'cfFoundWork'}
+            {text: "Schedule State", dataIndex: 'ScheduleState'},
+            {text: "Original Estimate", dataIndex: 'FirstPlanEstimate'},
+            {text: "Current Estimate", dataIndex: 'LastPlanEstimate'},
+            {text: "Delta", dataIndex: 'EstimateDelta' },
+            {text: 'cfEffortNotAccepted', dataIndex: 'cfEffortNotAccepted'}/*,
+            {text: 'cfFoundWork', dataIndex: 'cfFoundWork'} */
         ];
         Ext.Array.each( this.date_array, function(midnight){
             var stripped_date = midnight.replace(/T.*$/,"");
--- summary-model.js
+++ summary-model.js
@@ -4,9 +4,10 @@ Ext.define('Rally.pxs.data.SummaryModel',{
     fields: [
          { name: 'ObjectID', type: 'INT' },
          { name: 'Name', type: 'STRING' },
-         { name: 'FirstPlanEstimate', type: 'NUMBER', defaultValue: 0},
-         { name: 'LastPlanEstimate', type: 'NUMBER', defaultValue: 0},
-         { name: 'cfEffortNotAccepted', type: 'NUMBER', defaultValue:0},
+         { name: 'FirstPlanEstimate', type: 'NUMBER', defaultValue: 0 },
+         { name: 'LastPlanEstimate', type: 'NUMBER', defaultValue: 0 },
+         { name: 'EstimateDelta', type: 'NUMBER', defaultValue: 0 },
+         { name: 'cfEffortNotAccepted', type: 'NUMBER', defaultValue:0  },
          { name: 'cfFoundWork', type: 'NUMBER', defaultValue:0 },
          { name: 'ScheduleState', type: 'STRING', defaultValue: ""}
      ],
@@ -36,5 +37,20 @@ Ext.define('Rally.pxs.data.SummaryModel',{
             }
         });
         return snap;
+    },
+    set: function(fieldName, newValue) {
+        var me = this;
+        var changed_fields = this.callParent([fieldName, newValue]);
+        if (changed_fields !== null) {
+            if ( changed_fields.indexOf("FirstPlanEstimate") > -1 || changed_fields.indexOf("LastPlanEstimate") > -1 ){
+                var first = me.get('FirstPlanEstimate') || 0;
+                var last = me.get('LastPlanEstimate') || 0;
+                
+                var delta = last - first;
+                me.set('EstimateDelta',delta);
+                changed_fields.push('EstimateDelta');
+            }
+        }
+        return changed_fields;
     }
 });
